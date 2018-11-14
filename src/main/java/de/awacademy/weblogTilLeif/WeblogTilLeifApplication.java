ff8af703d6cddf92a89f30de18b0490c66358926
Leif Lampater <leiflampater@gmail.com>
2018-11-14 13:05:17 +0100

    Basic Home functionality

---
+++ src/main/java/de/awacademy/weblogTilLeif/WeblogTilLeifApplication.java
@@ -0,0 +1,12 @@
+package de.awacademy.weblogTilLeif;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeblogTilLeifApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeblogTilLeifApplication.class, args);
+	}
+}
---
+++ src/main/java/de/awacademy/weblogTilLeif/home/HomeController.java
@@ -0,0 +1,13 @@
+package de.awacademy.weblogTilLeif.home;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+	@GetMapping("/")
+	public String home(){
+		return "index";
+	}
+}
