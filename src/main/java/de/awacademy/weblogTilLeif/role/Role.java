7cf1c3b64a5099d1a93f2d71945979dd7c7c9890
Leif Lampater <leiflampater@gmail.com>
2018-11-16 12:35:36 +0100

    Revert "Not working with role repository"
    This reverts commit 81f7945

--- src/main/java/de/awacademy/weblogTilLeif/role/Role.java
+++ src/main/java/de/awacademy/weblogTilLeif/role/Role.java
@@ -1,26 +1,16 @@
 package de.awacademy.weblogTilLeif.role;
 
 import javax.persistence.Entity;
-import javax.persistence.Id;
 import javax.persistence.Table;
-import java.util.UUID;
 
 @Entity
 @Table(name = "Role")
 public class Role {
 
-    @Id
-    private String id;
-
-    private String role;
-
-    public String getId() {
-        return id;
-    }
+    String role;
 
     public Role(String role) {
         this.role = role;
-        this.id = UUID.randomUUID().toString();
     }
 
     public String getRole() {
