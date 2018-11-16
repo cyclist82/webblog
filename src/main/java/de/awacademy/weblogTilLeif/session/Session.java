b1bd92ddcef41ce53befa8125fea8bf6de839615
Leif Lampater <leiflampater@gmail.com>
2018-11-16 12:35:57 +0100

    Revert "ADD Role List; Constructor modified"
    This reverts commit f5cd47e

--- src/main/java/de/awacademy/weblogTilLeif/session/Session.java
+++ src/main/java/de/awacademy/weblogTilLeif/session/Session.java
@@ -32,6 +32,4 @@ public class Session {
 	public User getUser() {
 		return user;
 	}
-
-
 }
--- src/main/java/de/awacademy/weblogTilLeif/user/User.java
+++ src/main/java/de/awacademy/weblogTilLeif/user/User.java
@@ -1,18 +1,12 @@
 package de.awacademy.weblogTilLeif.user;
 
-
-
-
-import de.awacademy.weblogTilLeif.role.Role;
+import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.*;
-import java.util.*;
+import java.util.UUID;
 
 @Entity
 public class User {
-
-	@OneToMany
-	private List<Role> roles = new ArrayList<>();
 
 	@Id
 	private String id;
@@ -24,27 +18,10 @@ public class User {
 	public User() {
 	}
 
-	public List<Role> getRoles() {
-		return roles;
-	}
 
-	//	protected Set<Role> getRolesInternal() {
-//		if (this.roles == null) {
-//			this.roles = new HashSet<>();
-//		}
-//		return this.roles;
-//	}
-
-//	public List<Role> getPets() {
-//		List<Role> sortedRoles = new ArrayList<>(getRolesInternal());
-//		PropertyComparator.sort(sortedRoles,
-//				new MutableSortDefinition("name", true, true));
-//		return Collections.unmodifiableList(sortedRoles);
-//	}
 	public User(String username, String password) {
 		this.username = username;
 		this.password = password;
-		this.roles.add(new Role("user"));
 		this.id = UUID.randomUUID().toString();
 	}
 
