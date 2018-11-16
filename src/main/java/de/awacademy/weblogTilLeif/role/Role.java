81f794552326cbee27c2dbf024984feb847337f4
Til Tutschku <TilTutschku@gmx.de>
2018-11-16 11:38:04 +0100

    Not working with role repository

--- src/main/java/de/awacademy/weblogTilLeif/role/Role.java
+++ src/main/java/de/awacademy/weblogTilLeif/role/Role.java
@@ -1,16 +1,26 @@
 package de.awacademy.weblogTilLeif.role;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Table;
+import java.util.UUID;
 
 @Entity
 @Table(name = "Role")
 public class Role {
 
-    String role;
+    @Id
+    private String id;
+
+    private String role;
+
+    public String getId() {
+        return id;
+    }
 
     public Role(String role) {
         this.role = role;
+        this.id = UUID.randomUUID().toString();
     }
 
     public String getRole() {
